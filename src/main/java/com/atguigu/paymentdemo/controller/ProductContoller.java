e560a230acb16be0b822609212c0a1b3d6f70e52
guanjun <862418085@qq.com>
2022-05-15 17:04:13 +0800

    2022/5/15 15:31 微信支付项目

--- src/main/java/com/atguigu/paymentdemo/controller/ProductContoller.java
+++ src/main/java/com/atguigu/paymentdemo/controller/ProductContoller.java
@@ -1,10 +1,13 @@
 package com.atguigu.paymentdemo.controller;
 
+import com.atguigu.paymentdemo.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /*添加swagger设置*/
 @Api(tags = "商品管理")
@@ -18,5 +21,11 @@ public class ProductContoller {
     public String test() {
         System.out.println("测试");
         return "hello";
+    }
+    @ApiOperation("entity中Map-》data测试")
+    @GetMapping("/testR")
+    public R testR(){
+        System.out.println("new Date()"+new Date());
+        return R.ok().data("message","hello").data("now", new Date());
     }
 }
